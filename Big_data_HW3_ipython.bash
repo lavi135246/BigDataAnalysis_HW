e0537cde0a29fe621d863a91c4e683939aff8390
Mion <sakura5995@gmail.com>
2017-04-22 14:10:03 +0800

    Delete Big_data_HW3_ipython.bash

--- Big_data_HW3_ipython.bash
+++
@@ -1,9 +0,0 @@
-export SPARK_HOME=/opt/cloudera/parcels/CDH/lib/spark/
-
-#---- local -----#
-export PYSPARK_SUBMIT_ARGS="--master local[*] pyspark-shell"
-
-#---- yarn-client ----#
-export PYSPARK_SUBMIT_ARGS="--master yarn --deploy-mode client pyspark-shell"
-
-ipython notebook --profile=pyspark
